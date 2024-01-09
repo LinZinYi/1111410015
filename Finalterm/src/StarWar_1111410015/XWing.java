3f0c9778e5cab8a2e138d9b560edfdd8a8e896c5
林姿吟 <134450132+LinZinYi@users.noreply.github.com>
2024-01-09 17:22:26 +0800

    Add files via upload

--- Finalterm/src/StarWar_1111410015/XWing.java
+++ Finalterm/src/StarWar_1111410015/XWing.java
@@ -3,4 +3,10 @@ package StarWar_1111410015;
 public class XWing extends AirCraf {
 	static final String[] Shield = {"Chempat防禦者型偏轉幕發射器","Chempat偏轉防護盾投射器","鈦合金加勁外殼"};
 	private double[] ShieldPower = {2000,3000,1000};
+	public double[] getShieldPower() {
+		return ShieldPower;
+	}
+	public void setShieldPower(double[] shieldPower) {
+		ShieldPower = shieldPower;
+	}
 }
